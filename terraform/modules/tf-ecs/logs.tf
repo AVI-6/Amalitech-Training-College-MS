581f26b2330831675789c35c2bdd814b64732069
DonGranda <lesliekofiameg@gmail.com>
2026-04-21 10:19:40 +0000

    added: cloudwatch log stream for lambda and ecs was updated

--- terraform/modules/tf-ecs/logs.tf
+++ terraform/modules/tf-ecs/logs.tf
@@ -12,3 +12,14 @@ resource "aws_cloudwatch_log_stream" "cb_log_stream" {
   name           = "${var.name}-ecs-log-stream"
   log_group_name = aws_cloudwatch_log_group.log_group.name
 }
+
+
+resource "aws_cloudwatch_log_group" "ecs_notify_formatter" {
+  name = "/aws/lambda/${var.name}-ecs-notify-formatter"
+  retention_in_days = 14 
+}
+
+resource "aws_cloudwatch_log_stream" "ecs_notify_formatter_log_stream" {
+  name           = "${var.name}-ecs-notify-formatter"
+  log_group_name = aws_cloudwatch_log_group.ecs_notify_formatter.name
+}

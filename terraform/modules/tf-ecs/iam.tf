11b5d3bffcd42e0aac3a5f5ca708630b020bcdd2
DonGranda <lesliekofiameg@gmail.com>
2026-04-21 10:17:46 +0000

    execution role for lambda and role was added

--- terraform/modules/tf-ecs/iam.tf
+++ terraform/modules/tf-ecs/iam.tf
@@ -154,3 +154,43 @@ resource "aws_iam_role_policy" "ecs_auto_scale_custom_policy" {
     ]
   })
 }
+
+
+####Lambda execution role for ECS notifications
+resource "aws_iam_role" "ecs_notify_lambda_role" {
+  name = "${var.name}-ecs-notify-lambda-role" 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+
+resource "aws_iam_role_policy" "ecs_notify_lambda_policy" {
+  name = "${var.name}-ecs-notify-lambda-policy"
+  role = aws_iam_role.ecs_notify_lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+      }, 
+      {
+        Effect   = "Allow"
+        Action   = ["sns:Publish"]
+        Resource = aws_sns_topic.mail_notifications.arn
+      }
+    ]
+  })
+}
